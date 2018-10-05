865fd880d0b7f3a4698d54ab2fff3a76237d6849
zane-uw <zane@uw.edu>
2018-10-05 16:30:07 -0700

    more calcs

--- src/002_common courses analysis.R
+++ src/002_common courses analysis.R
@@ -46,47 +46,59 @@ transcripts %>%
 
 # Analyses ----------------------------------------------------------------
 
+# Decisions:
+# Filter cohort sizes into comparable ranges between FIG/non-FIG, 10-30
+#
+
+
 # — 1) Are FIG students taking courses together their first year? ---------
 #   1st year = terms 2 and 3, the winter/spring after the FIG
 #   took at least 1 other course section with a student from same FIG
 
-# filter, remove repeat courses, give students enough time to take more courses together
-dat <- transcripts %>% filter(term == 2 | term == 3, fig.yrq >= 20134, tran.yrq >= 20134) %>% distinct(fig.key, system_key, dept_abbrev, course_number, .keep_all = T)
+# initial subset = first wi/spr
+dat <- transcripts %>%
+  group_by(fig.key) %>%
+  mutate(fig.size = n_distinct(system_key)) %>%
+  ungroup() %>%
+  filter(fig.size >= 10, fig.size <= 30, term == 2 | term == 3, fig.yrq >= 20134, tran.yrq >= 20134) %>%
+  distinct(fig.key, system_key, dept_abbrev, course_number, section_id, .keep_all = T)
+# remove large sections (> 30 students)
+dat <- dat %>% group_by(term, cskey) %>% mutate(sect.size = n_distinct(system_key)) %>% filter(sect.size <= 30) %>% ungroup()
 
-# taking any sections together:
+# taking sections together:
 fig.sect <- dat %>%
   group_by(fig.key, term, dept_abbrev, course_number, section_id) %>%
-  mutate(n.sect = n()) %>%              # I may want to know the singletons later
+  mutate(n.fig.sect = n_distinct(system_key)) %>%              # I may want to know the singletons later; n() would work but why not be more specific
   ungroup() %>%
   group_by(fig.key, term, dept_abbrev, course_number) %>%  # also would like to know the course reg in case they take the course but not same section
-  mutate(n.course = n())
+  mutate(n.fig.course = n_distinct(system_key)) %>%
+  ungroup()
 
 
 fig.syskeys <- unique(dat$system_key)
-n1.classes <- unique(fig.sect$system_key[fig.sect$n.sect == 1])
-n2plus <- unique(fig.sect$system_key[fig.sect$n.sect >= 2])
+n1.classes <- fig.sect %>% filter(n.fig.sect == 1) %>% distinct(system_key)
+n2plus <- fig.sect %>% filter(n.fig.sect >= 2) %>% distinct(system_key)
 
 # so there were __ students that didn't take any sections with people from their fig
-singles <- n1.classes[!(n1.classes %in% n2plus)]
+singles <- setdiff(n1.classes$system_key, n2plus$system_key)
 length(singles)
-length(singles) / length(fig.syskeys)      # _not_ 95% after adjusting for registration sections
+length(singles) / length(fig.syskeys)      # _not_ 95% after adjusting for registration sections and getting rid of huge lectures
 
-(same.course.tab <- table('same course' = fig.sect$n.course, 'same sect' = fig.sect$n.sect))
-sum(diag(same.course.tab)) / sum(same.course.tab)
-
+(same.course.tab <- table('same course' = fig.sect$n.fig.course, 'same sect' = fig.sect$n.fig.sect))
 
 # verify with dplyr syntax:
-x <- fig.sect %>% group_by(fig.key) %>% filter(n.sect == 1) %>% distinct(fig.key, system_key) %>% ungroup()
-y <- fig.sect %>% group_by(fig.key) %>% filter(n.sect >= 2) %>% distinct(fig.key, system_key) %>% ungroup()
+x <- fig.sect %>% group_by(fig.key) %>% filter(n.fig.sect == 1) %>% distinct(fig.key, system_key) %>% ungroup()
+y <- fig.sect %>% group_by(fig.key) %>% filter(n.fig.sect >= 2) %>% distinct(fig.key, system_key) %>% ungroup()
 z <- anti_join(x, y)
 nrow(z) == length(singles)    # should be true
 rm(x, y, z)
+
 
 # common sections for proportions
 co.sect <- fig.sect %>% ungroup() %>%
   group_by(fig.key) %>%                   # noticed that add = F was possibly buggy before(?)
   mutate(nstu = n_distinct(system_key)) %>%
-  filter(n.sect >= 2) %>%
+  filter(n.fig.sect >= 2) %>%
   summarize(by.fig.nstu.any.joint.classes = n_distinct(system_key),
             fig.yrq = max(fig.yrq),
             fig.size = max(nstu)) %>%
@@ -114,8 +126,10 @@ length(unique(dat$fig.key[!(dat$fig.key %in% co.sect$fig.key)]))    # that few? 
 # w/ term
 pop.courses <- dat %>%
   group_by(fig.key, term) %>%
-  distinct(system_key, dept_abbrev, course_number, .keep_all = T) %>%
-  count(ckey, sort = T)
+  distinct(system_key, dept_abbrev, course_number, .keep_all = T) %>% ungroup()
+
+(p <- pop.courses %>% group_by(dept_abbrev, course_number) %>% summarize(pop = n_distinct(system_key)) %>% arrange(desc(pop)))
+
 
 # 3) Average # of sections a FIG student takes with someone else from the FIG
 # in first 2 years
@@ -131,22 +145,12 @@ table(stu.tally$n.sect)
 # of how many sections?
 (ns <- length(unique(stu.tally$cskey)))   # indeed, b/c people from different FIGs might appear in the same section(s)
 
-# stu.n <- stu.sub %>%
-#   group_by(fig.key, tran.yrq, system_key) %>%
-#   summarize(stu.n.sect = n_distinct(cskey),
-#             stu.n.course = n_distinct(ckey))
-
-
-
-
-
 
 
 # want to know what the average number of co-classes is -------------------
 
-# take sections from the fig.sect data
-co <- fig.sect %>%
-
+# by student, n.co >= 2, n distinct sections, mean
+x <- fig.sect %>% mutate(cut = if_else(n.fig.sect == 1, "1", "2+")) %>% group_by(system_key) %>% mutate(stu.n.courses = n_distinct())
 
 
 
@@ -164,45 +168,61 @@ not.fig$term <- unlist(tapply(not.fig$tran.yrq,
                                   }), use.names = F)
 
 
-# # first year
-# nf.cohort <- not.fig %>%
-#   filter(term == 1) %>%
-#   mutate(cohort = paste(yrq1, cskey, sep = "_")) %>%
-#   select(system_key, yrq1, cohort) %>%
-#   distinct()
-#
-# nf.yr1 <- not.fig %>%
-#   filter(term == 2 | term == 3) %>%
-#   left_join(nf.cohort)
+# # how many didn't come back in those terms?
+# length(unique(not.fig$system_key[(nf.cohort$system_key %in% nf.yr1$system_key) == F]))    # 618
 
-# how many didn't come back in those terms?
-length(unique(nf.cohort$system_key[(nf.cohort$system_key %in% nf.yr1$system_key) == F]))    # 618
-
-# co-classes w/ someone from same 'cohort' (section):
-nf.co.sect <- nf.yr1 %>%
-  group_by(cohort, cskey, add = F) %>%
-  summarize(by.sect.nstu.joint.classes = n_distinct(system_key))
-
-co.sect <- fig.sect %>%
-  group_by(fig.key, add = F) %>%
-  filter(n.sect >= 2) %>%
-  summarize(by.fig.nstu.any.joint.classes = n_distinct(system_key),
-            fig.yrq = max(fig.yrq),
-            fig.size = max(nstu)) %>%
-  mutate(joint.prop = by.fig.nstu.any.joint.classes / fig.size) %>%
+nfdat <- not.fig %>%
+  group_by(not.figkey) %>%
+  mutate(nf.size = n_distinct(system_key)) %>%
+  ungroup() %>%
+  filter(term == 2 | term == 3, nf.size >= 10, nf.size <= 30, yrq1 >= 20134, tran.yrq >= 20134) %>%
+  group_by(cskey) %>%
+  mutate(sect.size = n_distinct(system_key)) %>%
+  filter(sect.size <= 30) %>%
+  ungroup() %>%
+  group_by(not.figkey, cskey) %>%
+  mutate(n.common.sect = n_distinct(system_key)) %>%
+  ungroup() %>%
+  group_by(not.figkey, ckey) %>%
+  mutate(n.common.course = n_distinct(system_key)) %>%
   ungroup()
 
 
+nfco <- nfdat %>%
+  group_by(not.figkey, add = F) %>%
+  filter(n.common.sect >= 2) %>%
+  summarize(by.notfig.nstu.any.joint.classes = n_distinct(system_key)) %>%
+  ungroup()
 
-# other questions ---------------------------------------------------------
 
-# what are the most common/freq FIG courses?
-freq.courses <- transcripts %>% filter(term == 1, ckey != "0_GEN ST_199")
+# so the proportion of non-fig students who have any joint classes is:
+nf1 <- unique(nfdat$system_key[nfdat$n.common.sect == 1])
+nf2plus <- unique(nfdat$system_key[nfdat$n.common.sect > 1])
+nfsingles <- nf1[!(nf1 %in% nf2plus)]
 
-freq.courses <- freq.courses %>%
-  group_by(fig.yrq, ckey, cskey) %>%
-  distinct(fig.yrq, ckey, cskey) %>%
-  group_by(fig.yrq, ckey, add = F) %>%      # annual
-  summarize(nc = n())
+length(nfsingles) / length(unique(nfdat$system_key))
 
-freq.courses %>% group_by(ckey, add = F) %>% summarize(n = sum(nc)) %>% arrange(desc(n))
+
+
+
+# Section - to - section comparison ---------------------------------------
+
+fcse <- transcripts %>% filter(term == 1, dept_abbrev == "CSE", course_number == 142) %>% mutate(id = paste0(fig.yrq, dept_abbrev, course_number, section_id)) %>%
+  select(system_key, id) %>% distinct()
+
+nfcse <- not.fig %>% filter(term == 1, dept_abbrev == "CSE", course_number == 142) %>% mutate(id = paste0(tran.yrq, dept_abbrev, course_number, section_id)) %>%
+  select(system_key, id) %>% distinct()
+
+fcse <- fcse %>% inner_join(transcripts, by = "system_key") %>% filter(term == 2 | term == 3) # %>% select(id, term, tran.yrq, dept_abbrev, course_number, section_id)
+n <- not.fig %>% select(system_key, term, tran.yrq, dept_abbrev, course_number, section_id) %>% distinct()
+nfcse <- nfcse %>% inner_join(n, by = "system_key") %>% filter(term == 2 | term == 3) # %>% select(id, term, tran.yrq, dept_abbrev, course_number, section_id)
+
+x <- fcse %>% group_by(id, term, dept_abbrev, course_number, section_id) %>% summarize(n = n_distinct(system_key))
+y <- nfcse %>% group_by(id, term, dept_abbrev, course_number, section_id) %>% summarize(n = n_distinct(system_key))
+
+table(x$n)
+table(y$n)
+
+sum(x$n > 1) / nrow(x)
+sum(y$n > 1) / nrow(y)
+
